f9b2dfe5808685d409b2d33abe5c4f8474495fa1
GianniC88 <gianni.colucci.88@gmail.com>
2025-07-21 12:56:24 +0200

    Aggiungi Bootstrap e Bootstrap Icons, aggiorna il layout dell'app

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,34 @@ function App() {
   console.log(listItems);
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="text-center">
+              <h1>
+                <span className="me-2">🎬</span>Movies
+              </h1>
+            </div>
+            <div className="card">
+              <div className="card body">
+                <div className="row">
+                  <ul className="list-group list-group-flush p-3">
+                    <li className="list-group-item">An item</li>
+                    <li className="list-group-item">A second item</li>
+                    <li className="list-group-item">A third item</li>
+                    <li className="list-group-item">A fourth item</li>
+                    <li className="list-group-item">And a fifth one</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
