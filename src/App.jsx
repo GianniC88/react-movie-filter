5d7747a4715f1833248089d931d51955f70f2096
GianniC88 <gianni.colucci.88@gmail.com>
2025-07-21 12:01:34 +0200

    Aggiungi un array di film con titoli e generi

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import { useState } from "react";
 
 function App() {
+  const listItems = [
+    { title: "Inception", genre: "Fantascienza" },
+    { title: "Il Padrino", genre: "Thriller" },
+    { title: "Titanic", genre: "Romantico" },
+    { title: "Batman", genre: "Azione" },
+    { title: "Interstellar", genre: "Fantascienza" },
+    { title: "Pulp Fiction", genre: "Thriller" },
+  ];
+  console.log(listItems);
   const [count, setCount] = useState(0);
 
   return <></>;
