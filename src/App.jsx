8aa4d6c4e1bd5e5c94b3ae7b07dee927121691af
GianniC88 <gianni.colucci.88@gmail.com>
2025-07-21 18:09:11 +0200

    Ristruttura il componente App per gestire la ricerca dei film e aggiorna il layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { use } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const listItems = [
+  const listMovies = [
     { title: "Inception", genre: "Fantascienza" },
     { title: "Il Padrino", genre: "Thriller" },
     { title: "Titanic", genre: "Romantico" },
@@ -9,8 +10,14 @@ function App() {
     { title: "Interstellar", genre: "Fantascienza" },
     { title: "Pulp Fiction", genre: "Thriller" },
   ];
-  console.log(listItems);
-  const [count, setCount] = useState(0);
+  console.log(listMovies);
+
+  const [search, setSearch] = useState("");
+  const [filteredMovies, setFilteredMovies] = useState(listMovies);
+
+  useEffect(() => {
+    console.log("i amrunning no");
+  }, [search]);
 
   return (
     <>
@@ -23,14 +30,27 @@ function App() {
               </h1>
             </div>
             <div className="card">
-              <div className="card body">
+              <div className="card-body">
                 <div className="row">
+                  <form class="d-flex" role="search">
+                    <input
+                      className="form-control"
+                      type="search"
+                      placeholder="Cerca film..."
+                      value={search}
+                      onChange={(e) => setSearch(e.target.value)}
+                    />
+                    <button className="btn btn-outline-success" type="submit">
+                      Search
+                    </button>
+                  </form>
                   <ul className="list-group list-group-flush p-3">
-                    <li className="list-group-item">An item</li>
-                    <li className="list-group-item">A second item</li>
-                    <li className="list-group-item">A third item</li>
-                    <li className="list-group-item">A fourth item</li>
-                    <li className="list-group-item">And a fifth one</li>
+                    {listMovies.map((movie, index) => (
+                      <li key={index} className="list-group-item">
+                        <strong>{movie.title}</strong> -{" "}
+                        <span className="text-muted">{movie.genre}</span>
+                      </li>
+                    ))}
                   </ul>
                 </div>
               </div>
